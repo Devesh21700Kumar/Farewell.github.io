d6afff8833943b5fced185c3532317256d332f49
dmahajan980 <divyanshumahajan98@gmail.com>
2020-07-17 20:04:04 +0530

    Modified JSON function to replace images

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('.senior-link').click(function () {
         name = $(this).text().toLowerCase()
-        $.getJSON('../seniors/' + name + '/senior.json', function (senior) {
+        $.getJSON('../img/seniors/' + name + '/senior.json', function (senior) {
             document.title = 'Farewell ' + senior.name
             $('.carlo_txt').html('<h2>' + senior.h2 + '</h2>')
             $('.carlo_txt').append('<p>' + senior.p1_desc + '<br><br>' + senior.p2_desc + '</p>')
@@ -17,17 +17,41 @@ $(document).ready(function () {
             $('#div_patch2').html(senior.shortText2)
             $('.btn-play').attr('href', senior.TvVideoLink)
             $('.ravi_pic').attr('src', senior.section7bg)
-            $('.story1').attr('src', senior.swiperImages[0])
-            $('.s_story1').attr('src', senior.swiperImages[1])
-            $('.s_story2').attr('src', senior.swiperImages[2])
-            $('.s_story3').attr('src', senior.swiperImages[3])
-            $('.s_story4').attr('src', senior.swiperImages[4])
-            $('.s_story5').attr('src', senior.swiperImages[4])
-            $('.p_img1').attr('src', senior.swiperImages[0])
-            $('.p_img2').attr('src', senior.swiperImages[1])
-            $('.p_img3').attr('src', senior.swiperImages[2])
-            $('.p_img4').attr('src', senior.swiperImages[3])
-            $('.p_img5').attr('src', senior.swiperImages[4])
+
+            /**
+             * Carousel images
+             */
+            const swiperContainer = document.querySelector(".swiper-container");
+            
+            // Clear previous containers and create new ones
+            swiperContainer.innerHTML = "";
+            let swiperImages = senior.swiperImages,
+                swiperWrapper = document.createElement("div");
+            swiperWrapper.classList.add(".swiper-wrapper");
+
+            swiperImages.forEach(swiperImage => {
+                const swiperSlide = document.createElement("div"),
+                    imageWrapper = document.createElement("div"),
+                    carouselImage = document.createElement("img");
+            
+                // Add classes and other attributes for image
+                const imageClasses = ["s_story5", "class_disable", "animated", "animate_story_txt"];
+                carouselImage.classList.add(...imageClasses);
+                carouselImage.setAttribute("src", swiperImage);
+                carouselImage.setAttribute("alt", "poster");
+
+                // Add classes and other attributes for other containers
+                imageWrapper.classList.add("image-wrapper");
+                swiperSlide.classList.add("swiper-slide");
+
+                // Inject the elements in the respective order
+                imageWrapper.appendChild(carouselImage);
+                swiperSlide.appendChild(imageWrapper);
+                swiperWrapper.appendChild(swiperSlide);
+            });
+
+            // Inject the swiper wrapper into the container
+            swiperContainer.appendChild(swiperWrapper);
         })
     })
 })
