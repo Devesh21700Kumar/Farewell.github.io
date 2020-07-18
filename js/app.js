befe9014b53573b632a0c191f8a0bf52782b7c45
Yash Thakker <54551187+y-ash2001@users.noreply.github.com>
2020-07-18 16:39:02 +0530

    Updated the app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 over = document.getElementById('overlay');
 tog = document.getElementById('toggle');
 function toggle() {
-	console.log(1);
 	tog.classList.toggle('active');
 	over.classList.toggle('open');
 }
