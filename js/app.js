2f4ecc0bad53eb668d1f3015339b8bf8a5f71eda
Devesh <rockdev1234560@gmail.com>
2021-04-21 21:39:59 +0530

    the website

--- js/app.js
+++ js/app.js
@@ -1,6 +1,75 @@
 over = document.getElementById('overlay');
 tog = document.getElementById('toggle');
+e= document.getElementById('maropeetokutoBC');
+p1= document.getElementById('sexa');
+p2= document.getElementById('fuck');
+
+function toggle1() {
+	tog.classList.toggle('active');
+	over.classList.toggle('open');
+}
 function toggle() {
 	tog.classList.toggle('active');
 	over.classList.toggle('open');
+}
+
+function toggle2() {
+	tog.classList.toggle('active');
+	over.classList.toggle('open');
+}
+
+function toggle3() {
+	tog.classList.toggle('active');
+	over.classList.toggle('open');
+}
+
+function toggle4() {
+	tog.classList.toggle('active');
+	over.classList.toggle('open');
+}
+
+function toggle5() {
+	tog.classList.toggle('active');
+	over.classList.toggle('open');
+}
+
+function toggle6() {
+	tog.classList.toggle('active');
+	over.classList.toggle('open');
+}
+
+function  tag1(){
+	e.innerHTML="Ishant";
+	p2.innerText="Ishant";
+	p1.src="/img/seniors/aman/img/main.jpeg"
+}
+
+function  tag2(){
+	e.innerHTML="Arpit"; 
+	p2.innerText="calm and cool guy /kitno ne poocha IAS banoge?/ /kisi ne poocha nahi but ye question sabne poocha?/ ";
+	p1.src="/img/seniors/aman/img/4C.jpeg"
+}
+
+function  tag3(){
+	e.innerHTML="Samesh";
+	p2.innerText="Gsocer pro and Gopi Bahu Ultra pro max. Be the same gawd ever";
+	p1.src="/img/seniors/aman/img/3C.jpeg"
+}
+
+function  tag4(){
+	e.innerHTML="Arjun";
+	p2.innerText="Gawd as they say, CSA app pro guy, aise hi confident rehna";
+	p1.src="/img/seniors/aman/img/2C.jpeg"
+}
+
+function  tag5(){
+	e.innerHTML="Sarvesh";
+	p2.innerText="Lite lo so jaao is the GG code of honour for you. But when work arrives , fod dete ho. GG bakchod guy. ENJOY!!!";
+	p1.src="/img/seniors/aman/img/5C.jpeg"
+}
+
+function  tag6(){
+	e.innerHTML="Anshuman";
+	p2.innerText="Anshuman , the real gamdev gawd, The sincere cupa rustom. Campus pe zaroor milna";
+	p1.src="/img/seniors/aman/img/6C.jpeg"
 }
